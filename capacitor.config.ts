f01ba98d5d26b3018e01d574a2b8d025fdb178ab
affaled <affaledl@gmail.com>
2025-08-29 15:48:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'machine-shop-mobile-app',
+  webDir: 'www'
+};
+
+export default config;
