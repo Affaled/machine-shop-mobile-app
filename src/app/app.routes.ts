2e4955fe08d956b273da549751be798137a58daf
affaled <affaledl@gmail.com>
2025-09-02 12:06:20 -0300

    folder architecture modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    loadChildren: () =>
+      import('./pages/tabs/tabs.routes').then((m) => m.routes),
   },
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
 ];
---
+++ src/app/services/customer-repository/customer-repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from '../storage-service/storage-service';
+
+export interface Customer {
+  id: number;
+  name: string;
+  phone: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomerRepository {
+  private key = 'customers';
+
+  constructor(private storage: StorageService) {}
+
+  async getAll(): Promise<Customer[]> {
+    return (await this.storage.get<Customer[]>(this.key)) || [];
+  }
+
+  async getById(id: number): Promise<Customer | null> {
+    const customers = await this.getAll();
+    return customers.find((customer) => customer.id === id) || null;
+  }
+
+  async add(customer: Omit<Customer, 'id'>): Promise<Customer> {
+    const customers = await this.getAll();
+    const newCustomer: Customer = { id: Date.now(), ...customer };
+    customers.push(newCustomer);
+    await this.storage.set(this.key, customers);
+    return newCustomer;
+  }
+
+  async update(updatedCustomer: Customer): Promise<void> {
+    let customers = await this.getAll();
+    customers = customers.map((customer) =>
+      customer.id === updatedCustomer.id ? updatedCustomer : customer
+    );
+    await this.storage.set(this.key, customers);
+  }
+
+  async delete(id: number): Promise<void> {
+    const customers = await this.getAll();
+    await this.storage.set(
+      this.key,
+      customers.filter((customer) => customer.id !== id)
+    );
+  }
+}
