fddcc1fa3b32db9ad87019c98d5462324667d3e9
Victor Assunção <victor.a.schumann@gmail.com>
2025-09-07 05:00:05 -0300

    feat: adiciona métodos de crud ao service de order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { StorageService } from './storage.service';
 import { Order } from '../models/order.model';
 import { GenericRepository } from 'src/app/repositories/generic.repository';
@@ -8,12 +8,47 @@ import { GenericRepository } from 'src/app/repositories/generic.repository';
 })
 export class OrderService {
   private repository: GenericRepository<Order>;
+  private orders = signal<Order[]>([]);
+  public readonly orders$ = this.orders.asReadonly();
 
   constructor(private storage: StorageService) {
     this.repository = new GenericRepository<Order>(storage, 'orders');
+    this.initLoad();
   }
 
-  async getAllOrders(): Promise<Order[]> {
-    return this.repository.getAll();
+  private async initLoad() {
+    const list = await this.repository.getAll();
+    this.orders.set(list);
+  }
+
+  async addOrder(order: Order): Promise<void> {
+    await this.repository.add(order);
+    this.orders.update(o => [...o, order]);
+  }
+
+  async updateOrder(order: Order): Promise<void> {
+    await this.repository.update(order);
+    this.orders.update(o => {
+      const index = o.findIndex(item => item.id === order.id);
+      if (index !== -1) {
+        const updatedOrders = [...o];
+        updatedOrders[index] = order;
+        return updatedOrders;
+      }
+      return o;
+    });
+  }
+
+  async deleteOrder(id: string): Promise<void> {
+    await this.repository.delete(id);
+    this.orders.update(o => o.filter(item => item.id !== id));
+  }
+
+  async getById(id: string): Promise<Order | null> {
+    return await this.repository.getById(id);
+  }
+
+  async getAll(): Promise<Order[]> {
+    return await this.repository.getAll();
   }
 }
