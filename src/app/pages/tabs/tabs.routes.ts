82b68d9a490f9a86605abe651f2d23c0bbd1e94a
Victor Assunção <victor.a.schumann@gmail.com>
2025-09-07 03:05:02 -0300

    feat: adiciona rota para a tela de products

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -9,12 +9,17 @@ export const routes: Routes = [
       {
         path: 'motorcycle',
         loadComponent: () =>
-          import('../motorcycle/motorcycle.page').then((m) => m.MotorcyclePage),
+          import('../motorcycle/motorcycle.page').then((c) => c.MotorcyclePage),
       },
       {
         path: 'customer',
         loadComponent: () =>
-          import('../customers/customers.page').then((m) => m.CustomersPage),
+          import('../customers/customers.page').then((c) => c.CustomersPage),
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('../products/products.page').then((c) => c.ProductsPage),
       },
       {
         path: '',
